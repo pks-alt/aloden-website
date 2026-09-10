45d6d107824a07bbad3ffeb73f6bdd143c2cc6b9
pks-alt <pk.s@medlivo.com>
2026-09-09 17:44:19 -0700

    Load Company Section 2 review layer

--- preview/app.js
+++ preview/app.js
@@ -25,3 +25,9 @@ if (menuBtn && navLinks) {
     }
   });
 }
+
+if (document.querySelector('.companyHero')) {
+  const script = document.createElement('script');
+  script.src = 'company-section2.js?v=' + Date.now();
+  document.body.appendChild(script);
+}
